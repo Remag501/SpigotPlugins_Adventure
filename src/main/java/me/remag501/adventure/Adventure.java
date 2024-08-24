78cf20f040ac282041112f79e4b46bac773fe2f7
Vaibhav Kumar <therealvaibhavkumar10@gmail.com>
2024-08-24 02:45:40 -0700

    Adventure spigot maven project, first commit.

---
+++ src/main/java/me/remag501/adventure/Adventure.java
@@ -0,0 +1,17 @@
+package me.remag501.adventure;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Adventure extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
